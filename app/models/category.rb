1650fb0d141d6eb50640e7b8216b488268c4cf60
John Norman Capule <johnnormancapule@gmail.com>
2015-10-23 16:48:05 +0800

    1 searcheable

--- app/models/category.rb
+++ app/models/category.rb
@@ -8,16 +8,6 @@
 #
 
 class Category < ActiveRecord::Base
-  include Elasticsearch::Model
-  include Cacheable
-
-  settings index: { number_of_shards: 1 } do
-    mappings do
-      indexes :name
-      indexes :created_at
-    end
-  end
-
   has_many :articles, dependent: :destroy
 end
 
