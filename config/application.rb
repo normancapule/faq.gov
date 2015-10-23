ea07ece4582a03486016a54de073e55259d316bb
John Norman Capule <johnnormancapule@gmail.com>
2015-10-23 14:12:50 +0800

    api controllers

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,10 @@ Bundler.require(*Rails.groups)
 module FaqGov
   class Application < Rails::Application
     MultiJson.engine = :Oj
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'controllers', '{**}')]
     config.active_record.raise_in_transactional_callbacks = true
+    config.session_store :disabled
     config.generators do |g|
       g.template_engine nil #to skip views
       g.test_framework  nil #to skip test framework
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
     scope module: :v1 do
       resource :session, only: %i(show create destroy)
       resource :registration, only: %i(create)
+      resources :categories do
+        resources :articles
+      end
+      resources :articles do
+        resources :comments
+        resources :tags
+        resources :tips
+      end
     end
   end
 
